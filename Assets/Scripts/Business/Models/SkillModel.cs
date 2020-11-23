84d0f289ca4b1a2e9243b344c9b06d42336be4b5
Toloymak <brovko-a@live.com>
2020-11-23 21:57:03 +0500

    Staring to work with Unit model

---
+++ Assets/Scripts/Business/Models/SkillModel.cs
@@ -0,0 +1,10 @@
+﻿using Business.Enums;
+
+namespace Business.Models
+{
+    public class SkillModel
+    {
+        public SkillType Type { get; set; }
+        public int Experience { get; set; }
+    }
+}
---
+++ Assets/Scripts/Business/Models/UnitModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using Business.Enums;
+
+namespace Business.Models
+{
+    public class UnitModel
+    {
+        public Guid Id { get; }
+        // todo: Implementation for few cults
+        public Guid CultId { get; set; }
+        
+        public string Name { get; set; }
+        
+        // Classic parameters
+        public int MaxHealth { get; set; }
+        public int CurrentHealth { get; set; }
+        
+        public int MaxEnergy { get; set; }
+        public int CurrentEnergy { get; set; }
+
+        public int Strength { get; set; }
+        public int Intelligence { get; set; }
+        public int Agile { get; set; }
+        public int Endurance { get; set; }
+        
+        // Element parameters
+        public int FireChi { get; set; }
+        public int ColdChi { get; set; }
+        public int WaterChi { get; set; }
+        public int AirChi { get; set; }
+        public int EarthChi { get; set; }
+        public int DeathChi { get; set; }
+        
+        public int BodyChi { get; set; }
+        
+        public IDictionary<SkillType, SkillModel> Skills { get; }
+
+        public UnitModel()
+        {
+            Id = Guid.NewGuid();
+            CultId = Guid.Empty;
+            Skills = new Dictionary<SkillType, SkillModel>();
+        }
+    }
+}
