e73c506c46cee77c63e40c20d9bf43a4eaaa8607
Toloymak <brovko-a@live.com>
2020-11-24 21:52:20 +0500

    Add timer and Start/Stop logic

---
+++ Assets/Scripts/Common/Components/TimerComponent.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Diagnostics.SymbolStore;
+
+namespace Common.Components
+{
+    public class TimerComponent
+    {
+        private bool _isActive;
+        private TimeSpan _timeBeforeStop;
+
+        private DateTime _lastStart;
+
+        public bool IsActive => _isActive;
+
+        public TimerComponent()
+        {
+            _timeBeforeStop = TimeSpan.Zero;
+        }
+        
+        public void Stop()
+        {
+            _isActive = false;
+            _timeBeforeStop += _lastStart - DateTime.Now;
+        }
+
+        public void Start()
+        {
+            _isActive = true;
+            _lastStart = DateTime.Now;
+        }
+    }
+}
---
+++ Assets/Scripts/Common/Systems/Timer/TimerSystem.cs
@@ -0,0 +1,45 @@
+﻿using Common.Components;
+using Leopotam.Ecs;
+using UnityEngine;
+
+namespace Common.Systems.Timer
+{
+    public class TimerSystem: BaseSystem, IEcsInitSystem, IEcsRunSystem
+    {
+        private EcsWorld _ecsWorld = null;
+
+        private TimerComponent _timerComponent;
+        private bool _isFirstFrame;
+
+        public void Init()
+        {
+            _timerComponent = _ecsWorld.NewEntity().Set<TimerComponent>();
+            _isFirstFrame = true;
+        }
+
+        public void Run()
+        {
+            if (_isFirstFrame)
+            {
+                _isFirstFrame = false;
+                _timerComponent.Start();
+            }
+
+            var isPushedStopButton = Input.GetKeyDown(KeyCode.Space);
+
+            if (!isPushedStopButton) return;
+            
+            switch (_timerComponent.IsActive)
+            {
+                case true:
+                    LogService.AddLog("Stop");
+                    _timerComponent.Stop();
+                    break;
+                case false:
+                    LogService.AddLog("Start");
+                    _timerComponent.Start();
+                    break;
+            }
+        }
+    }
+}
