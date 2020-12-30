871510e11e2863cf71fe9f78a9a36977b4cdeea3
Toloymak <brovko-a@live.com>
2020-12-30 13:49:49 +0500

    Added new unity project for 3d

---
+++ Cult3d/Assets/Scripts/MainManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Services;
+using UnityEngine;
+
+public class MainManager : MonoBehaviour
+{
+    private CameraControlService _cameraControlService;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _cameraControlService = new CameraControlService();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _cameraControlService.MoveCamera();
+    }
+}
---
+++ Cult3d/Assets/Scripts/Services/CameraControlService.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+namespace Services
+{
+    public class CameraControlService
+    {
+        private const float CameraSpeed = 10f;
+        
+        public void MoveCamera()
+        {
+            MoveCamera(GetDirectionVector());
+        }
+
+        private static void MoveCamera(Vector3 vector3)
+        {
+            if (vector3 != default && !(Camera.main is null))
+                Camera.main.transform.position += vector3 * CameraSpeed * Time.deltaTime;
+        }
+
+        private static Vector3 GetDirectionVector()
+        {
+            var vector3 = new Vector3();
+
+            if (Input.GetKey("up"))
+                vector3 += Vector3.forward;
+
+            if (Input.GetKey("down"))
+                vector3 += Vector3.back;
+
+            if (Input.GetKey("right"))
+                vector3 += Vector3.right;
+
+            if (Input.GetKey("left"))
+                vector3 += Vector3.left;
+            return vector3;
+        }
+    }
+}
