ef770a20d837935916b3fa17618e6f375f285432
Toloymak <brovko-a@live.com>
2021-04-05 23:14:22 +0500

    Add blazor site for debug

---
+++ Core/Web/WebServices/TimeHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Web.WebServices
+{
+    public static class TimeHelper
+    {
+        private static DateTime _dateTime;
+        private static object _lock = new object();
+        
+        private static DateTime _lastProcedureTime = DateTime.Now;
+
+        public static TimeSpan GetDeltaTime()
+        {
+            lock (_lock)
+            {
+                var delta = DateTime.Now - _lastProcedureTime;
+                _lastProcedureTime = DateTime.Now;
+                return delta;
+            }
+        }
+    }
+}
